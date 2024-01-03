ae698191a61b2f5308f622fc58e3951c73e5b74b
Arbaj Shaikh <118815119+Arbajsshaikh@users.noreply.github.com>
2024-01-03 17:28:31 +0530

    Update new.py

--- new.py
+++ new.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Assuming your data is stored in the 'filtered_data' DataFrame
 # If not, replace 'filtered_data' with your DataFrame name
-URL = 'DIST_Franchise-Orders-2022-23.zip'
+URL='DIST_Franchise-Orders-2022-23.zip'
 # For example, you can load data from a CSV file using pd.read_csv('your_data.csv')
 filtered_data = pd.read_csv(URL)
 
@@ -12,7 +12,7 @@ district_dropdown = st.selectbox('Select District:', filtered_data['DISTRICT'].u
 
 # Create a function to update the Shop-Code options based on the selected District
 def update_shop_code_options(selected_district):
-    shop_code_options = filtered_data.query(f'DISTRICT == "{selected_district}"')['Shop-Code'].unique()
+    shop_code_options = filtered_data.loc[filtered_data['DISTRICT'] == selected_district]['Shop-Code'].unique()
     return ['All'] + list(shop_code_options)
 
 # Create a dropdown widget for selecting a Shop-Code
@@ -23,9 +23,10 @@ if st.button('Update Shop-Code Options'):
     shop_code_dropdown.options = update_shop_code_options(district_dropdown)
 
 # Display the Medicine Names and Quantities based on the selected Shop-Code
-selected_data = filtered_data.query(
-    f'DISTRICT == "{district_dropdown}" & ("All" == "{shop_code_dropdown}" | `Shop-Code` == "{shop_code_dropdown}")'
-)
+if shop_code_dropdown != 'All':
+    selected_data = filtered_data.loc[(filtered_data['DISTRICT'] == district_dropdown) & (filtered_data['Shop-Code'] == shop_code_dropdown)]
+else:
+    selected_data = filtered_data.loc[filtered_data['DISTRICT'] == district_dropdown]
 
 # Sort the data by Qty in descending order
 selected_data = selected_data.sort_values(by='Qty', ascending=False)
